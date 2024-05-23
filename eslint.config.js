30650ded58aaf684ab26f9d9e6ceb7b6694187eb
Mateus Azevedo <mateusazevedoaguiar@gmail.com>
2024-05-22 21:55:22 -0300

    implementando testes com jest e react-test-library

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,11 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    extends: ["react-app", "react-app/jest"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
---
+++ src/Components/List/test.spec.ts
@@ -0,0 +1,5 @@
+describe("teste1", () => {
+  it("deve retornar verdadeiro quando 1 for igual a 1", () => {
+    expect(1).toBe(1);
+  });
+});
